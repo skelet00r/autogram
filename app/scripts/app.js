926e6f4259657437b02551ca074aecbf29ffe042
ratzilla <jdelibas@gmail.com>
2014-07-28 02:39:49 +0100

    removed starter app comments

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,6 @@
 'use strict';
-// Ionic Starter App
+/* global StatusBar */
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('Autogram', ['ionic','Autogram-Controllers','Autogram-Directives','Autogram-Services'])
 
 .run(function($ionicPlatform) {
